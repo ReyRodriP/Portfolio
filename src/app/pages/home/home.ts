c7d23e619521ace68a7d5930b95d9caa00b1a4fe
ReyRodriP <reynaldorodriguez802@gmail.com>
2026-09-12 15:07:18 -0400

    Creacion de intro

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Intro } from '../../shared/intro/intro';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Intro],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+  isAnimating = true;
 
+  @HostListener('document:click')
+  dispararAnimacion() {
+    this.isAnimating = false;
+
+    setTimeout(() => {
+      this.isAnimating = true;
+    }, 50)
+  }
 }
